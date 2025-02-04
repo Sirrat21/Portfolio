5df5c26176e360b7c8f8b5bda13b2a92247fb479
Sirrat21 <sirratrashid@gmail.com>
2025-02-04 21:12:04 +0500

    Updated feature XYZ

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,7 @@ const Contact = () => {
     return (
         <div
             name="contact"
-            className="w-full min-h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
-        >
+        className="w-full min-h-screen bg-gradient-to-b from-gray-800 to-black text-white">
             <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
                 <div className="pb-8">
                     <p className="text-4xl font-bold inline border-b-4 border-gray-500">
@@ -17,7 +16,7 @@ const Contact = () => {
                     <form
                         action="https://getform.io/f/bmdpqmoa"
                         method="POST"
-                        className="flex flex-col w-full max-w-md mx-auto" // Updated
+                        className="flex flex-col w-full max-w-md mx-auto" 
                     >
                         <input
                             type="text"
